fa838bac6f0603a2d96e2a15885d884d1295ac87
Rain Ramm <rammrain@gmail.com>
2020-03-06 14:21:30 +0200

    Improve BusinessException constructor, fix release task

--- src/main/java/ee/bitweb/core/exception/BusinessException.java
+++ src/main/java/ee/bitweb/core/exception/BusinessException.java
@@ -2,6 +2,10 @@ package ee.bitweb.core.exception;
 
 public class BusinessException extends RuntimeException {
 
+    public BusinessException(String message) {
+        super(message);
+    }
+
     public BusinessException(String message, Throwable cause) {
         super(message, cause);
     }
