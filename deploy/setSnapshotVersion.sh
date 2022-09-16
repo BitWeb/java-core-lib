2a637899acc04bb3fe02c9514277c64af7bdb36c
Rain Ramm <rain@bitweb.ee>
2022-09-16 13:12:26 +0300

    Fix version replace for snapshot builds

--- deploy/setSnapshotVersion.sh
+++ deploy/setSnapshotVersion.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -o errexit
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  echo "Not meant to be used on Mac OS!"
+  exit 1
+fi
+
 BUILD_NUMBER=$1
 CURRENT=$(./deploy/getVersion.sh)
 
@@ -8,4 +13,4 @@ NEW_VERSION=$(./deploy/getVersion.sh)-SNAPSHOT-$BUILD_NUMBER
 
 echo "Change version from $CURRENT to $NEW_VERSION"
 
-sed -i "" "s/version '$CURRENT'/version '$NEW_VERSION'/" build.gradle >> build.gradle
+sed -i "s/version '$CURRENT'/version '$NEW_VERSION'/" build.gradle >> build.gradle
