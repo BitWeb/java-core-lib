bbe7273b5b0f0fcab28888e28640cf65b2905175
Rain Ramm <rain@bitweb.ee>
2024-05-21 15:36:21 +0300

    Add SilencedGelfTcpAppender to silence GELF connection issues (#17)

---
+++ src/main/java/ee/bitweb/core/logging/SilencedGelfTcpAppender.java
@@ -0,0 +1,36 @@
+package ee.bitweb.core.logging;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import de.siegmar.logbackgelf.GelfTcpAppender;
+import ee.bitweb.core.exception.CoreException;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SilencedGelfTcpAppender extends GelfTcpAppender {
+
+    @Override
+    protected void append(final ILoggingEvent event) {
+        final byte[] binMessage = getEncoder().encode(event);
+
+        try {
+            appendMessage(binMessage);
+        } catch (CoreException ignored) {
+            // Catching and ignoring CoreException which will be thrown if application can't connect to Graylog, because we don't want the application
+            // to stop.
+        }
+    }
+
+    @Override
+    protected void appendMessage(final byte[] messageToSend) {
+        try {
+            super.appendMessage(messageToSend);
+        } catch (CoreException e) {
+            throw new CoreException("Error sending GELF message", e);
+        }
+    }
+
+    @Override
+    public void addError(String msg, Throwable ex) {
+        throw new CoreException(msg, ex);
+    }
+}
