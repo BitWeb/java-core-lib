2cbc8da991452b7f17b63ec1fe99f9972750461b
Enno Eller <enno.eller@gmail.com>
2025-04-15 12:16:52 +0300

    Retrofit response body mapper fix (#44)
    Co-authored-by: ennoeller <enno@bitweb.ee>

--- src/main/java/ee/bitweb/core/retrofit/logging/mappers/RetrofitResponseBodyMapper.java
+++ src/main/java/ee/bitweb/core/retrofit/logging/mappers/RetrofitResponseBodyMapper.java
@@ -66,7 +66,6 @@ public class RetrofitResponseBodyMapper implements RetrofitLoggingMapper {
     private String parseBody(Response response) throws IOException {
         var responseBody = response.body();
         var contentType = responseBody.contentType();
-        var contentLength = responseBody.contentLength();
         var charset = contentType != null ? contentType.charset(UTF_8) : UTF_8;
 
         var source = responseBody.source();
@@ -84,18 +83,16 @@ public class RetrofitResponseBodyMapper implements RetrofitLoggingMapper {
             return "(binary %s-byte body omitted)".formatted(buffer.size());
         }
 
-        if (contentLength != 0L) {
-            var bodyString = buffer.clone().readString(charset);
-            if (contentLength > maxLoggableResponseSize) {
-                return "%s ... Content size: %s characters".formatted(
-                        bodyString.substring(0, maxLoggableResponseSize),
-                        contentLength
-                );
-            }
+        var bodyString = buffer.clone().readString(charset);
+        var bodyStringLength = bodyString != null ? bodyString.length() : 0;
 
-            return sanitizeBody(bodyString);
+        if (bodyStringLength > maxLoggableResponseSize) {
+            return "%s ... Content size: %s characters".formatted(
+                    bodyString.substring(0, maxLoggableResponseSize),
+                    bodyStringLength
+            );
         } else {
-            return "";
+            return sanitizeBody(bodyString);
         }
     }
 }
